5abde5ed0bd2636b3f9012336e69b7357059f12b
Gonzalo Recio <gon@reciogarcia.com>
2015-11-26 10:18:19 +0100

    cocktail scaffold

---
+++ db/migrate/20151126091748_create_cocktails.rb
@@ -0,0 +1,10 @@
+class CreateCocktails < ActiveRecord::Migration
+  def change
+    create_table :cocktails do |t|
+      t.string :name
+      t.text :body
+
+      t.timestamps null: false
+    end
+  end
+end
